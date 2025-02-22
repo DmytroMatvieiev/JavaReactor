ce42ae7cf15cf28ce9adf3329f9e7473e91c5905
Dmytro Matvieiev <matvievievie@gmail.com>
2025-02-22 15:31:24 +0200

    revert to origin

--- src/main/java/org/dmdev/javareact/JavaReactApplication.java
+++ src/main/java/org/dmdev/javareact/JavaReactApplication.java
@@ -1,9 +1,6 @@
 package org.dmdev.javareact;
 
-import org.dmdev.javareact.publisher.PublisherImpl;
-import org.dmdev.javareact.subscriber.SubscriberImpl;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 //@SpringBootApplication
 public class JavaReactApplication {
@@ -13,24 +10,5 @@ public class JavaReactApplication {
 
     }
 
-    private static void demo1(){
-        var publisher = new PublisherImpl();
-        var subscriber = new SubscriberImpl();
-        publisher.subscribe(subscriber);
-    }
-
-    private static void demo2(){
-        var publisher = new PublisherImpl();
-        var subscriber = new SubscriberImpl();
-        publisher.subscribe(subscriber);
-        subscriber.getSubscription().request(3);
-    }
-
-    private static void demo3(){
-        var publisher = new PublisherImpl();
-        var subscriber = new SubscriberImpl();
-        publisher.subscribe(subscriber);
-        subscriber.getSubscription().request(24);
-    }
 
 }
