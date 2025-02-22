8910ce678e0f6d0cb4d9cc9c0d8692ea72bceab0
Dmytro Matvieiev <matvievievie@gmail.com>
2025-02-22 14:33:54 +0200

    initial commit

---
+++ src/test/java/org/dmdev/javareact/JavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmdev.javareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
