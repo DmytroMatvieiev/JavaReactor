5e9a5398b3e20e6f2931e51750ec65b426540bee
Dmytro Matvieiev <matvievievie@gmail.com>
2025-02-22 15:26:53 +0200

    publisher/subscriber implementation

--- src/main/java/org/dmdev/javareact/JavaReactApplication.java
+++ src/main/java/org/dmdev/javareact/JavaReactApplication.java
@@ -1,13 +1,36 @@
 package org.dmdev.javareact;
 
+import org.dmdev.javareact.publisher.PublisherImpl;
+import org.dmdev.javareact.subscriber.SubscriberImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class JavaReactApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaReactApplication.class, args);
+   //     SpringApplication.run(JavaReactApplication.class, args);
+
+    }
+
+    private static void demo1(){
+        var publisher = new PublisherImpl();
+        var subscriber = new SubscriberImpl();
+        publisher.subscribe(subscriber);
+    }
+
+    private static void demo2(){
+        var publisher = new PublisherImpl();
+        var subscriber = new SubscriberImpl();
+        publisher.subscribe(subscriber);
+        subscriber.getSubscription().request(3);
+    }
+
+    private static void demo3(){
+        var publisher = new PublisherImpl();
+        var subscriber = new SubscriberImpl();
+        publisher.subscribe(subscriber);
+        subscriber.getSubscription().request(24);
     }
 
 }
